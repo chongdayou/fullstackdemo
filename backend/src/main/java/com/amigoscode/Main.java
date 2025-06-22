5989f8496bceda2f6f2388f750f96d0f0386e5e1
Chongda You <74991409+chongdayou@users.noreply.github.com>
2025-06-21 21:15:54 -0500

    feat: set up config and upload/get service from AWS S3.

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Random;
@@ -24,6 +25,7 @@ public class Main {
     }
 
     @Bean
+    @Profile("!Test")
     CommandLineRunner runner(
             CustomerRepository customerRepository,
             PasswordEncoder passwordEncoder,
--- backend/src/main/java/com/amigoscode/s3/S3Config.java
+++ backend/src/main/java/com/amigoscode/s3/S3Config.java
@@ -3,6 +3,8 @@ package com.amigoscode.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,6 +18,7 @@ public class S3Config {
     public S3Client s3Client() {
         S3Client client = S3Client.builder()
                 .region(Region.of(awsRegion))
+                .httpClientBuilder(ApacheHttpClient.builder())
                 .build();
         return client;
     }
